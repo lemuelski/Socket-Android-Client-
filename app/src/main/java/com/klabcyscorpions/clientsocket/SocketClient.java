86ac9f6cf04c2d4ef4449ab0658aec3a289101a4
Lemuel Castro <l.castro@klab.com>
2016-01-07 12:20:44 +0800

    initial commit

---
+++ app/src/main/java/com/klabcyscorpions/clientsocket/SocketClient.java
@@ -0,0 +1,166 @@
+package com.klabcyscorpions.clientsocket;
+
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SocketClient extends AppCompatActivity {
+
+    private TextView connection_status, server_messages;
+    private Button btn_connect, btn_disconnect, btn_send;
+    private Socket socket = null;
+    private boolean is_connected = false;
+
+    private View.OnClickListener btn_connection_listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_connect:
+                    connection_status.setText("Connecting");
+                        new MyClientTask("10.3.15.185", 4000).execute();
+                    break;
+                case R.id.btn_disconnect:
+                    connection_status.setText("Disconnected");
+                    if (socket != null) {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+            }
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_socket_client);
+
+        connection_status = (TextView) findViewById(R.id.connection_status);
+        server_messages = (TextView) findViewById(R.id.server_messages);
+        btn_connect = (Button) findViewById(R.id.btn_connect);
+        btn_disconnect = (Button) findViewById(R.id.btn_disconnect);
+        btn_send = (Button)findViewById(R.id.btn_send);
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessageToServer();
+            }
+        });
+
+        btn_connect.setOnClickListener(btn_connection_listener);
+        btn_disconnect.setOnClickListener(btn_connection_listener);
+    }
+
+    public class MyClientTask extends AsyncTask<Void, Void, Void> {
+
+        String dstAddress;
+        int dstPort;
+        String response = "";
+
+        MyClientTask(String addr, int port) {
+            dstAddress = addr;
+            dstPort = port;
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            Log.i("lem", "start");
+            try {
+                socket = new Socket(dstAddress, dstPort);
+
+              /*  ByteArrayOutputStream byteArrayOutputStream =
+                        new ByteArrayOutputStream(1024);
+                byte[] buffer = new byte[1024];*/
+
+                /*int bytesRead;
+                InputStream inputStream = socket.getInputStream();*/
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String message = " ";
+                /*
+                 * notice:
+                 * inputStream.read() will block if no data return
+                 */
+
+                while((message = in.readLine())!=null){
+                    updateMessagesFromClient(message);
+                }
+
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                response = "UnknownHostException: " + e.toString();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                response = "IOException: " + e.toString();
+            } finally {
+                is_connected = socket != null;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.i("lem", "post");
+            connection_status.setText(is_connected ? "Connected" : "Disconnected");
+        }
+
+    }
+
+    private void sendMessageToServer(){
+            Log.i("lem",((EditText)findViewById(R.id.edittext_toserver)).getText().toString());
+            new Handler().post(new Runnable(){
+                @Override
+                public void run() {
+                    try {
+                        PrintStream printStream = new PrintStream(socket.getOutputStream());
+                        printStream.println(((EditText) findViewById(R.id.edittext_toserver)).getText().toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+    }
+
+    private void updateMessagesFromClient(final String message){
+        if(message.equals("fart")){
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.fart);
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+            mp.start();
+        }
+
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                server_messages.setText(server_messages.getText() + "\n" + message);
+            }
+        });
+    }
+
+}
